148d481730aeaa2752deb7f39dbcd19a2a8fcecd
Kyle McFadden <burntwist@sbcglobal.net>
2019-07-08 00:10:35 -0700

    wrote the object variable data for the question/answers and some simple html to get started

---
+++ assets/javascript/app.js
@@ -0,0 +1,102 @@
+
+
+// begin by creating an array to hold the objects that hold the questions and answer data
+
+var questionArray = [
+        { question: "How many numbered Final Fantasy games have been released to this date?",
+            answers: {
+                a: "13",
+                b: "14",
+                c: "15",
+                d: "16",
+            },
+            correct: "c"
+        },
+        { question: "In what year did the original Atari 2600 console launch?",
+            answers: {
+                a: "1980",
+                b: "1977",
+                c: "1976",
+                d: "1981",
+            },
+            correct: "b"
+        },
+        { question: "What video game holds the lowest Metacritic score?",
+            answers: {
+                a: "Family Party: Obstacle Arcade",
+                b: "Ride to Hell: Retribution",
+                c: "Leisure Suit Larry: Box Office Bust",
+                d: "Vroom in the Night Sky",
+            },
+            correct: "a"
+        },
+        { question: "What video game holds the record time for longest development?",
+            answers: {
+                a: "Diablo 3",
+                b: "Duke Nukem Forever",
+                c: "The Last Guardian",
+                d: "Final Fantasy XV",
+            },
+            correct: "b"
+        },
+        { question: "What is currently the fastest selling video game console?",
+            answers: {
+                a: "Playstation 2",
+                b: "Nintendo 3DS",
+                c: "Xbox 360",
+                d: "Nintendo Switch",
+            },
+            correct: "d"
+        },
+        { question: "How many characters appeared as playable units in Capcom's original Project X Zone?",
+            answers: {
+                a: "60",
+                b: "63",
+                c: "64",
+                d: "62",
+            },
+            correct: "a"
+        },
+        { question: "Which video game console has the most high scoring (90+) exclusives on Metacritic?",
+            answers: {
+                a: "Playstation 3",
+                b: "Xbox 360",
+                c: "Nintendo 64",
+                d: "Playstation 2",
+            },
+            correct: "d"
+        },
+        { question: "What is the oldest still-running MMORPG?",
+            answers: {
+                a: "The Realm Online",
+                b: "Kingdom of Drakkar",
+                c: "Meridian 59",
+                d: "Furcadia",
+            }, 
+            correct: "b"
+        },
+        { question: 'William "B.J." Blazkowicz is a main character of which game series?',
+            answers: {
+                a: "Wolfenstein",
+                b: "Call of Duty",
+                c: "Medal of Honor",
+                d: "Sniper Elite",
+            },
+            correct: "a"
+        },
+        { question: "What is the best selling video game of all time?",
+            answers: {
+            a: "Minecraft",
+            b: "Wii SPorts",
+            c: "Tetris",
+            d: "Grand Theft Auto V",
+            },
+            correct: "c"
+        }
+    ];
+
+console.log(questionArray)
+console.log(questionArray[5].answers)
+
+
+
