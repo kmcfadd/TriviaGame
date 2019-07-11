dd4d7183bed36b5b716fb1a7bea9ee8c1d0dacf5
kmcfadd <39887165+kmcfadd@users.noreply.github.com>
2019-07-11 07:56:44 -0700

    scrapped the code and started over with a constructor function method for defining the questions and answers

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,140 +2,60 @@
 
 $(document).ready(function(){
 
-// begin by creating an array to hold the objects that hold the questions and answer data
+// write a constructor function to define the objects for the questions
 
-var questionArray = [
-        { question: "How many numbered Final Fantasy games have been released to this date?",
-            answers: {
-                a: "13",
-                b: "14",
-                c: "15",
-                d: "16",
-            },
-            correct: "c"
-        },
-        { question: "In what year did the original Atari 2600 console launch?",
-            answers: {
-                a: "1980",
-                b: "1977",
-                c: "1976",
-                d: "1981",
-            },
-            correct: "b"
-        },
-        { question: "What video game holds the lowest Metacritic score?",
-            answers: {
-                a: "Family Party: Obstacle Arcade",
-                b: "Ride to Hell: Retribution",
-                c: "Leisure Suit Larry: Box Office Bust",
-                d: "Vroom in the Night Sky",
-            },
-            correct: "a"
-        },
-        { question: "What video game holds the record time for longest time in development?",
-            answers: {
-                a: "Diablo 3",
-                b: "Duke Nukem Forever",
-                c: "The Last Guardian",
-                d: "Final Fantasy XV",
-            },
-            correct: "b"
-        },
-        { question: "What is currently the fastest selling video game console?",
-            answers: {
-                a: "Playstation 2",
-                b: "Nintendo 3DS",
-                c: "Xbox 360",
-                d: "Nintendo Switch",
-            },
-            correct: "d"
-        },
-        { question: "How many characters appeared as playable units in Capcom's original Project X Zone?",
-            answers: {
-                a: "60",
-                b: "63",
-                c: "64",
-                d: "62",
-            },
-            correct: "a"
-        },
-        { question: "Which video game console has the most high scoring (90+) exclusives on Metacritic?",
-            answers: {
-                a: "Playstation 3",
-                b: "Xbox 360",
-                c: "Nintendo 64",
-                d: "Playstation 2",
-            },
-            correct: "d"
-        },
-        { question: "What is the oldest still-running MMORPG?",
-            answers: {
-                a: "The Realm Online",
-                b: "Kingdom of Drakkar",
-                c: "Meridian 59",
-                d: "Furcadia",
-            }, 
-            correct: "b"
-        },
-        { question: 'William "B.J." Blazkowicz is a main character of which game series?',
-            answers: {
-                a: "Wolfenstein",
-                b: "Call of Duty",
-                c: "Medal of Honor",
-                d: "Sniper Elite",
-            },
-            correct: "a"
-        },
-        { question: "What is the best selling video game of all time?",
-            answers: {
-            a: "Minecraft",
-            b: "Wii SPorts",
-            c: "Tetris",
-            d: "Grand Theft Auto V",
-            },
-            correct: "c"
-        }
-    ];
-
-console.log(questionArray)
-console.log(questionArray[5].answers)
-
-var questionContainer = document.getElementById('question');
-var answersContainer = document.getElementById('answers');
-var resultContainer = document.getElementById('result');
-
-function startGame(){
-    $("#directions").addClass('none');
-    $("#start").addClass('none');
-
-    var output = [];
-
-    questionArray.forEach(
-        (currentQuestion, questionNumber) => {
-
-            var answers = [];
-
-            for(letter in currentQuestion.answers){
-
-                answers.push(
-                    `<label>
-                    <input type="radio" name="question${questionNumber}" value="${letter}">
-                    ${letter} :
-                    ${currentQuestion.answers[letter]}
-                    </label>`
-                );
-            }
-
-            output.push(
-                `<div class="question"> ${currentQuestion.question} </div>
-                <div class="answers"> ${answers.join('')} </div>`
-            )
-    }
-  );
-
-  questionContainer.innerHTML = output.join('');
+function Question(text, choices, answer) {
+    this.text = text;
+    this.choices = choices;
+    this.answer = answer;
 }
 
-$("#start").click(startGame);
+// defining the condition from the parent constructor to which an answer is correct
+
+Question.prototype.isCorrect = function(choice) {
+    return this.answer === choice;
+}
+
+
+/*  create an array with the objects that hold the questions and answer data
+in reference to the constructor function */
+
+var questions = [
+        new Question("How many numbered Final Fantasy games have been released to this date?",
+            ["13","14","15","16"],"15"),
+
+        new Question("In what year did the original Atari 2600 console launch?",
+            ["1980","1977","1976","1981"],"1977"),
+
+        new Question("What video game holds the lowest Metacritic score?",
+             ["Family Party: Obstacle Arcade", "Ride to Hell: Retribution",
+             "Leisure Suit Larry: Box Office Bust","Vroom in the Night Sky"],
+             "Family Party: Obstacle Arcade"),
+
+        new Question("What video game holds the record time for longest time in development?",
+           ["Diablo 3", "Duke Nukem Forever", "The Last Guardian", "Final Fantasy XV"],
+           "Duke Nukem Forever"),
+
+        new question("What is currently the fastest selling video game console?",
+            ["Playstation 2", "Nintendo 3DS", "Xbox 360", "Nintendo Switch"], "Nintendo Switch"),
+
+        new Question("How many characters appeared as playable units in Capcom's original Project X Zone?",
+            ["60", "63", "64", "62"], "60"),
+
+        new Question("In what year was the Sega Dreamcast discontinued?",
+            ["1998", "1999", "2000", "2001"], "2001"),
+
+        new Question("What is the oldest still-running MMORPG?",
+            ["The Realm Online", "Kingdom of Drakkar", "Meridian 59", "Furcadia"], "Kingdom of Drakkar"),
+
+        new Question('William "B.J." Blazkowicz is a main character of which game series?',
+            ["Wolfenstein", "Call of Duty", "Medal of Honor", "Sniper Elite"], "Wolfenstein"),
+
+        new Question("What is the best selling video game of all time?",
+            ["Minecraft", "Wii SPorts", "Tetris", "Grand Theft Auto V"], "Tetris")
+    ];
+
+
+
 
 });
